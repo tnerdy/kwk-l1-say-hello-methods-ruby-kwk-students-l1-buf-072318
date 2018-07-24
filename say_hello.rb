9c1b09d2119cadb1b19af116e566692b35ee00aa
Teresa Rakowsky <teresarakowsky@gmail.com>
2018-07-24 14:31:36 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,8 +1,8 @@
 # Build your say_hello method here
 
 def say_hello(your_name,my_name)
- puts "Hello #{your_name}! im #{my_name}
+ puts "Hello #{your_name}! im #{my_name}"
 end
   
 
-
+say_hello("minka","teresa")
